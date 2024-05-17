557d58c100b250bfa779235fc9888fbfb076fc10
Rakibul Anam <rakibul.anam@brainstation-23.com>
2024-05-17 17:13:19 +0600

    Initial Commit

---
+++ BSMovieCenter/BSMovieCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BSMovieCenterApp.swift
+//  BSMovieCenter
+//
+//  Created by BS00834 on 17/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BSMovieCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BSMovieCenter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BSMovieCenter
+//
+//  Created by BS00834 on 17/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
